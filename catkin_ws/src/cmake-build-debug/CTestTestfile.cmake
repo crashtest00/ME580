dbbfbace0fd9613932c49b840797091e628ca16d
crashtest84 <jjmarck@gmail.com>
2017-12-04 22:29:23 -0600

    Remove cmake-build-debug

--- catkin_ws/src/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jack/catkin_ws/src
-# Build directory: /home/jack/catkin_ws/src/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("beginner_tutorials")
